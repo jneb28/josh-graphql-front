7d3d74bb9dc7c36e1571dba7d952aac7fcb2a757
Josh <jn.web.developer@gmail.com>
2019-04-07 21:13:11 -0600

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,9 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
-    <v-toolbar app></v-toolbar>
-    <v-content>
-      <v-container fluid>
-        <router-view>
-          <HelloWorld/>
-        </router-view>
-      </v-container>
-    </v-content>
-    <v-footer app></v-footer>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld'
-//import axios from 'axios'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
-  }
-  // mounted () {
-  //   axios({
-  //     url: 'http://localhost:4000/',
-  //     method: 'post',
-  //     data: {
-  //       query: `
-  //         query {
-  //           players {
-  //             name
-  //           }
-  //         }
-  //         `
-  //     }
-  //   }).then((result) => {
-  //     console.log(result.data)
-  //   });
-  // }
-}
-</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import './plugins/vuetify'
 import App from './App.vue'
+import router from './router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
