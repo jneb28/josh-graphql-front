106821b03ab80044aac341f72a9affee8e5a75fd
Josh <jn.web.developer@gmail.com>
2019-04-18 13:26:07 -0600

    forms, tabs, update & delete mutations

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,36 +1,112 @@
 <template>
   <div id="players">
-    <div>
-      <form>
-        <div>
-          <span>Name:</span>
-          <input type="text" name="Name" v-model="name">
-        </div>
-        <div>
-          <span>Wins:</span>
-          <input type="text" name="Wins" v-model="wins">
-        </div>
-        <div>
-          <span>Losses:</span>
-          <input type="text" name="Losses" v-model="losses">
-        </div>
-        <div>
-          <span>Race:</span>
-          <input type="text" name="Race" v-model="race">
-        </div>
-        <div>
-          <span>Realm ID:</span>
-          <input type="text" name="Realm" v-model="id">
-        </div>
-      </form>
-      <button @click="createPlayer">Create Player</button>
-    </div>
+    <v-layout>
+      <v-flex xs6>
+        <v-tabs slider-color="grey lighten-4">
+          <v-tab color="">Create</v-tab>
+          <v-tab-item>
+            <v-card flat>
+              <v-form>
+                <v-container>
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="name" label="Player name"></v-text-field>
+                    </v-flex>
+                  </v-layout>
 
-    <p>Name: {{ name }}</p>
-    <p>Wins: {{ wins }}</p>
-    <p>Losses: {{ losses }}</p>
-    <p>Race: {{ race }}</p>
-    <p>Realm ID: {{ id }}</p>
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="wins" label="Player wins"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="losses" label="Player losses"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="race" label="Player race"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="realmId" label="Realm ID"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-form>
+              <v-btn @click="createPlayer">Submit</v-btn>
+            </v-card>
+          </v-tab-item>
+
+          <v-tab>Delete</v-tab>
+          <v-tab-item>
+            <v-card flat>
+              <v-form>
+                <v-container>
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="playerId" label="Player ID"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-form>
+              <v-btn @click="deletePlayer">Submit</v-btn>
+            </v-card>
+          </v-tab-item>
+
+          <v-tab>Update</v-tab>
+          <v-tab-item>
+            <v-card flat>
+              <v-form>
+                <v-container>
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="playerId" label="Player ID"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="name" label="Player name"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="wins" label="Player wins"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="losses" label="Player losses"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="race" label="Player race"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+
+                  <v-layout>
+                    <v-flex xs12>
+                      <v-text-field v-model="realmId" label="Realm ID"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-form>
+              <v-btn @click="updatePlayer">Submit</v-btn>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </v-flex>
+    </v-layout>
 
     <ul>
       <li v-for="player in players" :key="player._id">{{ player.name }}</li>
@@ -49,8 +125,8 @@ export default {
       wins: 0,
       losses: 0,
       race: "",
-      id: ""
-      
+      playerId: "",
+      realmId: ""
     };
   },
   methods: {
@@ -58,11 +134,11 @@ export default {
       this.$apollo
         .mutate({
           mutation: gql`
-           mutation CreatePlayer($data: PlayerCreateInput!) {
-             createPlayer(data: $data) {
-               _id
-             }
-           }
+            mutation CreatePlayer($data: PlayerCreateInput!) {
+              createPlayer(data: $data) {
+                _id
+              }
+            }
           `,
           variables: {
             data: {
@@ -72,23 +148,102 @@ export default {
               race: this.race,
               realm: {
                 connect: {
-                  _id: this.id
+                  _id: this.realmId
                 }
               }
             }
           }
         })
         .then(result => {
+          this.$apollo.queries.AllPlayers.refetch();
           console.log(result.data);
         })
         .catch(error => {
           console.log(error);
         });
+
+        this.name = "";
+        this.wins = 0;
+        this.losses = 0;
+        this.race = "";
+        this.realmId = "";
+    },
+    deletePlayer() {
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation DeletePlayer($where: PlayerWhereUniqueInput!) {
+              deletePlayer(where: $where) {
+                _id
+              }
+            }
+          `,
+          variables: {
+            where: {
+              _id: this.playerId
+            }
+          }
+        })
+        .then(result => {
+          this.$apollo.queries.players.refetch();
+          console.log(result.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+        this.name = "";
+        this.wins = 0;
+        this.losses = 0;
+        this.race = "";
+        this.playerId = "";
+    },
+    updatePlayer() {
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation UpdatePlayer($data: PlayerUpdateInput!, $where: PlayerWhereUniqueInput!) {
+              updatePlayer(data: $data, where: $where) {
+                _id
+              }
+            }
+          `,
+          variables: {
+            data: {
+              name: this.name,
+              wins: Number(this.wins),
+              losses: Number(this.losses),
+              race: this.race,
+              realm: {
+                connect: {
+                  _id: this.realmId
+                }
+              }
+            },
+            where: {
+              _id: this.playerId
+            }
+          }
+        })
+        .then(result => {
+          this.$apollo.queries.players.refetch();
+          console.log(result.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+        this.playerId = "";
+        this.name = "";
+        this.wins = 0;
+        this.losses = 0;
+        this.race = "";
+        this.realmId = "";
     }
   },
   apollo: {
     players: gql`
-      query {
+      query GetAllPlayers {
         players {
           name
         }
